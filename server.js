7368327ab243667b18576c5f7712bd4d33149ae5
MaoriD <maori@MacBook-Pro-de-Dechaine.local>
2018-02-02 14:16:06 +0100

    Ajout du serveur

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set( 'views', __dirname + '/www' ); 
+app.use( express.static(path.join(__dirname, 'www')) );
+
+const frontRoute = require('./routes/front');
+const apiRoute = require('./routes/api');
+
+app.use('/', frontRoute);
+app.use('/api', apiRoute);
+
+app.engine( 'html', ejs.renderFile );
+app.set( 'view engine', 'html' );
+
+app.listen( port, () => console.log(`Le serveur est lancé sur le port ${port}`) );
